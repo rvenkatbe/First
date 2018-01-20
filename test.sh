6fb164f21d8b58ddcbcfb5a07bec95ec8562042a
rvenkatbe <rvenkatbe@yahoo.com>
2018-01-20 11:40:14 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,21 +1,2 @@
-echo "Working on Pre-Check" | mailx -s "Ansible POC: Pre-Check" "venkadasalam.rajasekaran@selectcomfort.com"
+
 /d01/ansible/zero_call.sh
-sleep 10s
-echo "Working on Preparation" | mailx -s "Ansible POC: Preparation" "venkadasalam.rajasekaran@selectcomfort.com"
-/d01/ansible/first_call.sh
-sleep 10s
-echo "Working on Stage" | mailx -s "Ansible POC: Stage" "venkadasalam.rajasekaran@selectcomfort.com"
-/d01/ansible/second_call.sh
-sleep 10s
-echo "Working on Move" | mailx -s "Ansible POC: Move" "venkadasalam.rajasekaran@selectcomfort.com"
-/d01/ansible/third_call.sh
-sleep 10s
-echo "Working on Verify" | mailx -s "Ansible POC: Verify" "venkadasalam.rajasekaran@selectcomfort.com"
-/d01/ansible/fourth_call.sh
-sleep 10s
-echo "Working on Finish" | mailx -s "Ansible POC: Finish" "venkadasalam.rajasekaran@selectcomfort.com"
-/d01/ansible/fifth_call.sh
-
-
-
-
